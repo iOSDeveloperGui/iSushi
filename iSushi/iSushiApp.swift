57c3deb2cee46a787f92bd2d5a0cc1cf3712e7f6
iOSDeveloperGui <guilhermesilvadias535@gmail.com>
2025-12-15 00:10:25 -0300

    Initial Commit

---
+++ iSushi/iSushiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSushiApp.swift
+//  iSushi
+//
+//  Created by iOS Developer on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iSushiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
